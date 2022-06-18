3f7e27fe13cf34f70e329115f7a7af9d381261f3
Asif <aumi.asif@gmail.com>
2022-06-18 23:12:16 +0600

    filter service created

--- Server/src/filter/filter.controller.ts
+++ Server/src/filter/filter.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+} from '@nestjs/common';
+import { FilterService } from './filter.service';
 
-@Controller('filter')
-export class FilterController {}
+import { FilterDto } from './../../dto/filter.dto';
+@Controller('filters')
+export class FilterController {
+  constructor(
+    private filterService: FilterService,
+  ) {}
+
+  @Post()
+  create(@Body() dto: FilterDto) {
+    return this.filterService.create(dto);
+  }
+}
--- Server/src/filter/filter.module.ts
+++ Server/src/filter/filter.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FilterService } from './filter.service';
+import { FilterController } from './filter.controller';
 
 @Module({
-  providers: [FilterService]
+  providers: [FilterService],
+  controllers: [FilterController],
 })
 export class FilterModule {}
