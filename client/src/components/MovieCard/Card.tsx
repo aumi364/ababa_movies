f081b9b90c50816c73c36b49a386814c959939d4
Asif <aumi.asif@gmail.com>
2022-06-21 01:36:42 +0600

    card style upgraded

--- client/src/components/MovieCard/Card.tsx
+++ client/src/components/MovieCard/Card.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import style from "./CardStyle.module.css";
 
-type Props = {};
+type Props = {
+  image: string;
+  description: string;
+  filter?: any;
+  name: string;
+};
 
-const Card = (props: Props) => {
-  return <div>Card</div>;
+const Card = ({ image, description, filter, name }: Props) => {
+  return (
+    <div className={style.cardContainer}>
+      <div className={style.cardImage}>
+        <img src={image} alt="poster" />
+      </div>
+      <div className={style.cardBody}>
+        <div className={style.cardName}>
+          <h3>{name}</h3>
+        </div>
+      </div>
+      {/* <div className={style.rating}>{filter.rating}</div> */}
+    </div>
+  );
 };
 
 export default Card;
--- client/src/pages/home/Movies/MovieListContainer.tsx
+++ client/src/pages/home/Movies/MovieListContainer.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "../../../app/store";
+import Card from "../../../components/MovieCard/Card";
 import { getMovies } from "../../../slices/movieSlice";
+import style from "./Movies.module.css";
 
 type Props = {};
 
@@ -11,10 +13,22 @@ const MovieListContainer = (props: Props) => {
   //   console.log(movieState);
 
   useEffect(() => {
-    dispatch(getMovies({ url: "/movies" }));
+    dispatch(getMovies({ url: "/movies?limit=2&offset=2" }));
   }, []);
-
-  return <div>MovieListContainer</div>;
+  console.log(data);
+  return (
+    <div className={style.movieListContainer}>
+      {data?.map((movie: any) => {
+        return (
+          <Card
+            image={movie?.imageUrl}
+            description={movie?.description}
+            name={movie?.name}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default MovieListContainer;
